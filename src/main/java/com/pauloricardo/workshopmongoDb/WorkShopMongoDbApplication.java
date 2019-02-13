9cbe7f20c590afdff90129d08dc2a766d26423f8
prmsantos1112 <prmsantosster@gmail.com>
2019-02-13 14:37:44 -0200

    Project Create MongoDb Spring Boot

---
+++ src/main/java/com/pauloricardo/workshopmongoDb/WorkShopMongoDbApplication.java
@@ -0,0 +1,14 @@
+package com.pauloricardo.workshopmongoDb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopMongoDbApplication.class, args);
+	}
+
+}
+
